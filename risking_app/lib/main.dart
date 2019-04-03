111c15a6934319cf679fc2335097d4a111e5dd10
Marc <43479593+MORATA22@users.noreply.github.com>
2019-04-03 19:46:43 +0200

    I HAVE A NAVIGATOR

--- risking_app/lib/main.dart
+++ risking_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'splash_screen_one.dart';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(
   title: 'Risking',
@@ -10,3 +11,53 @@ void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner :false,
   home:SplashScreenOne(),
 ));
+
+
+class  SplashScreenOne extends StatefulWidget {
+  @override
+  _SplashScreenOneState createState() => _SplashScreenOneState();
+}
+class _SplashScreenOneState extends State<SplashScreenOne> {
+
+  @override
+    void initState(){
+      super.initState();
+      Timer(Duration(seconds: 5),()=> Navigator.push(context, MaterialPageRoute(builder: (context)=>null)));
+    }
+
+
+  @override
+  Widget build(BuildContext context){    
+    return Scaffold(
+      body:Stack(
+        fit:StackFit.expand,
+        children: <Widget>[
+          Container(            
+            decoration: BoxDecoration(
+              color: new Color(0xff689f38),             
+            ),
+          ),
+        Column(mainAxisAlignment: MainAxisAlignment.center,
+        
+          children: <Widget>[          
+          CircleAvatar(backgroundColor: Colors.blue,
+          radius:50.0,
+          child: Icon(
+            Icons.map,
+            color: Colors.deepOrange,
+            size: 30.0,
+          ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0)
+            ),
+            Text('Cargando',
+            style: TextStyle(color:Colors.blue,
+            fontSize: 24.0),
+            ),
+        ],)
+        ]
+      ),
+    );
+  }
+}
